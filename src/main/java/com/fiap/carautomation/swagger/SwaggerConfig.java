2d2088090321e5e4f0cee954eceaa0dcbe824b4c
Victor <victorluiz.monteiro@gmail.com>
2018-05-14 01:35:17 -0300

    Atualizando Swagger.

--- src/main/java/com/fiap/carautomation/swagger/SwaggerConfig.java
+++ src/main/java/com/fiap/carautomation/swagger/SwaggerConfig.java
@@ -29,12 +29,12 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     }
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
-                .title("Spring Boot REST API")
-                .description("\"Spring Boot REST API for Online Store\"")
+                .title("Car Automation  REST API")
+                .description("\"MBA - FIAP\"")
                 .version("1.0.0")
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-                .contact(new Contact("John Thompson", "https://springframework.guru/about/", "john@springfrmework.guru"))
+                .contact(new Contact("Victor Monteiro", "", "victorluiz.monteiro@gmail.com"))
                 .build();
     }
     @Override
